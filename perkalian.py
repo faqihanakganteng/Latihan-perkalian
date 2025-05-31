eb421cddb35d68e576c6ffc00079fdaa441db67f
Qihh <81425211+faqihanakganteng@users.noreply.github.com>
2025-05-31 17:10:07 +0700

    Update perkalian.py
    [bahasa]
    + Penambahan pengulangan 
    
    [English]
    + Added repetition

--- perkalian.py
+++ perkalian.py
@@ -1,17 +1,17 @@
 import random
 
-nomor1 = random.randint(1, 50)
-nomor2 = random.randint(1, 10)
-
-kali = nomor1 * nomor2
-
-username = input("Masukan nama kamu: ")
-
+username = input("Masukkan nama kamu: ")
 print(f"Selamat datang {username}! Ayo latihan perkalian bersamaku!")
 
-pilihan_user = int(input(f"Berapa hasil dari {nomor1} × {nomor2}? : "))
+ulang = True
+while ulang:
+    nomor1 = random.randint(1, 50)
+    nomor2 = random.randint(1, 10)
 
-if pilihan_user == kali:
-    print("Selamat kamu benar!")
-else:
-    print(f"Yaaah..... Kamu salah, Hasil dari {nomor1} × {nomor2} adalah {kali}")
+    kali = nomor1 * nomor2
+    pilihan_user = int(input(f"Berapa hasil dari {nomor1} × {nomor2}? : "))
+
+    if pilihan_user == kali:
+        print("Selamat kamu benar!")
+    else:
+        print(f"Yaaah..... Kamu salah, Hasil dari {nomor1} × {nomor2} adalah {kali}")
